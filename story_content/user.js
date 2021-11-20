c98647574535688e959b4e7c44c029ef5ded0928
Shoffzehra <ahmad.iqbal.1603116@students.um.ac.id>
2021-11-20 15:03:37 +0700

    first commit

---
+++ story_content/user.js
@@ -0,0 +1,543 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5udXAWGVzdP":
+        Script1();
+        break;
+      case "6kzbSbgTZt7":
+        Script2();
+        break;
+      case "5Yo86FdcNka":
+        Script3();
+        break;
+      case "6PBfhVowipx":
+        Script4();
+        break;
+      case "6miqobf2szR":
+        Script5();
+        break;
+      case "6MEtzz2PC4B":
+        Script6();
+        break;
+      case "6Sv0hI2H4gp":
+        Script7();
+        break;
+      case "5rLqw2Jeiit":
+        Script8();
+        break;
+      case "6oSfCXpopkj":
+        Script9();
+        break;
+      case "5UlyD7Fnsrn":
+        Script10();
+        break;
+      case "6blkhk5OJ9G":
+        Script11();
+        break;
+      case "6SwbfJESaPP":
+        Script12();
+        break;
+      case "6PZktgWLR6M":
+        Script13();
+        break;
+      case "5oBy50gR1E9":
+        Script14();
+        break;
+      case "60CKd2zFh4M":
+        Script15();
+        break;
+      case "6Yub3hF2HGB":
+        Script16();
+        break;
+      case "6JVOmpJZYl2":
+        Script17();
+        break;
+      case "5gzqubs4qNr":
+        Script18();
+        break;
+      case "6DDS7K3NT8K":
+        Script19();
+        break;
+      case "69kflAWR81l":
+        Script20();
+        break;
+      case "5ewpbZIlIe7":
+        Script21();
+        break;
+      case "5Z9l8Ky10h2":
+        Script22();
+        break;
+      case "6UvYcztpMlu":
+        Script23();
+        break;
+      case "5ZOqAQVPYOD":
+        Script24();
+        break;
+      case "6cl8b7KyugE":
+        Script25();
+        break;
+      case "6NWrUOwYioE":
+        Script26();
+        break;
+      case "5wnLAxIfw8K":
+        Script27();
+        break;
+      case "5wQ7gNbO8zM":
+        Script28();
+        break;
+      case "5yKYnGBnnVK":
+        Script29();
+        break;
+      case "6likWKo3ocv":
+        Script30();
+        break;
+      case "6elfOQZIBay":
+        Script31();
+        break;
+      case "6hSYz2fwnAk":
+        Script32();
+        break;
+      case "5cLrraVkQFf":
+        Script33();
+        break;
+      case "5t9uSh4mKNe":
+        Script34();
+        break;
+      case "6JkcKyOSCbm":
+        Script35();
+        break;
+      case "6f1jRwu4FC2":
+        Script36();
+        break;
+      case "5smmbc5wAwS":
+        Script37();
+        break;
+      case "6EqwiadYsn2":
+        Script38();
+        break;
+      case "6IxjmhG1jSV":
+        Script39();
+        break;
+      case "6MZvHdcOrJn":
+        Script40();
+        break;
+      case "6bdwpHHlnOi":
+        Script41();
+        break;
+      case "5h8zEP9tVKx":
+        Script42();
+        break;
+      case "5uOHY1WaUQe":
+        Script43();
+        break;
+      case "6DQMVezQBZM":
+        Script44();
+        break;
+      case "6Iubct69R2T":
+        Script45();
+        break;
+      case "5y7KrGYMkI8":
+        Script46();
+        break;
+      case "657gyjnDBIP":
+        Script47();
+        break;
+      case "64GypcxqaJZ":
+        Script48();
+        break;
+      case "6M1pXYyWjJL":
+        Script49();
+        break;
+      case "6cQi0XF3pWd":
+        Script50();
+        break;
+      case "5lpJxEU7efA":
+        Script51();
+        break;
+      case "5rsC3v4VazV":
+        Script52();
+        break;
+      case "6YuUatQuQsV":
+        Script53();
+        break;
+      case "5WpRHMSd5hI":
+        Script54();
+        break;
+      case "6PbuR6djyEx":
+        Script55();
+        break;
+      case "5vZZ7sDQ6qO":
+        Script56();
+        break;
+      case "6WsNu2BdEjc":
+        Script57();
+        break;
+      case "65lIU9zoL0a":
+        Script58();
+        break;
+      case "6J9lDJR1wte":
+        Script59();
+        break;
+      case "5eXFEMPX5KZ":
+        Script60();
+        break;
+      case "5mlzm1nJLLs":
+        Script61();
+        break;
+      case "6TWAZiqaPJj":
+        Script62();
+        break;
+      case "6OBBqXm0K5E":
+        Script63();
+        break;
+      case "6mZ8mjvKOOc":
+        Script64();
+        break;
+      case "5hkThD5116G":
+        Script65();
+        break;
+      case "6WyNIC1jiPD":
+        Script66();
+        break;
+      case "5rS6fmyJ8k0":
+        Script67();
+        break;
+  }
+}
+
+function Script1()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script2()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script3()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script4()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script5()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script6()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script7()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script8()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script9()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script10()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script11()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script12()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script13()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script14()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script15()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script16()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script17()
+{
+  parent.document.body.style.backgroundColor = "#D9D9D9";
+}
+
+function Script18()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script19()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script20()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script21()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script22()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script23()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script24()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script25()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script26()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script27()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script28()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script29()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script30()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script31()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script32()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script33()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script34()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script35()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script36()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script37()
+{
+  parent.document.body.style.backgroundColor = "#1E3137";
+}
+
+function Script38()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script39()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script40()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script41()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script42()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script43()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script44()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script45()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script46()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script47()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script48()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script49()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script50()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script51()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script52()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script53()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script54()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script55()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script56()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script57()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script58()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script59()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script60()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script61()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script62()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script63()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script64()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script65()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script66()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function Script67()
+{
+  parent.document.body.style.backgroundColor = "#FFFFFF";
+}
+
